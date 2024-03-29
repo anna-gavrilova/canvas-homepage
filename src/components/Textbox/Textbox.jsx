4e57311ea0b252ce6ebde29fadcc007e1b01d570
Anna Gavrilova <agavrilova@MBPe23JQ1H454QLJ.local>
2024-03-28 20:55:57 -0400

    Added POC for selecting the shape

--- src/components/Textbox/Textbox.jsx
+++ src/components/Textbox/Textbox.jsx
@@ -1,11 +1,12 @@
 import { Text } from 'react-konva';
 
 const TextBox = (props) => {
-    const { id, isEditing, dragObject } = props;
+    const { id, isEditing, dragObject, isSelected, onSelect } = props;
     return (
         <Text
-        text="this is my sample text"
+        text={isSelected ? "I am selected" : "this is my sample text"}
         draggable={isEditing}
+        onClick={onSelect}
         onDragStart={(e) => {
           console.log('starting position',e.target,e.target.x(), e.target.y());
         }}
